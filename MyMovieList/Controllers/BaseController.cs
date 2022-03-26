da2a4451924c3082f8f05b4e2566d5f58e1d855c
Pavel1112 <Pavel1112@students.softuni.bg>
2022-03-26 16:42:05 +0200

    Login and register views changed

---
+++ MyMovieList/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMovieList.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
