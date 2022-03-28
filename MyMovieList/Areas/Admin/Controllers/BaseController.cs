761baf2d2820e82d996eb99f590df591568debf3
Pavel1112 <Pavel1112@students.softuni.bg>
2022-03-28 16:14:51 +0300

    Area attribute added

--- MyMovieList/Areas/Admin/Controllers/BaseController.cs
+++ MyMovieList/Areas/Admin/Controllers/BaseController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MyMovieList.Areas.Admin.Controllers
 {
     [Authorize(Roles ="Administrator")]
+    [Area("Admin")]
     public class BaseController : Controller
     {
     }
