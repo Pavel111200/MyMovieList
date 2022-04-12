31778590e752a019139cb8fc5eae8b38032cfba2
Pavel1112 <Pavel1112@students.softuni.bg>
2022-04-12 12:00:39 +0300

    All movies added to user

---
+++ MyMovieList/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMovieList.Core.Contracts;
+
+namespace MyMovieList.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieService movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            this.movieService = movieService;
+        }
+        public async Task<IActionResult> AllMovies()
+        {
+            var model = await movieService.GetAllMovies();
+
+            return View(model);
+        }
+    }
+}
